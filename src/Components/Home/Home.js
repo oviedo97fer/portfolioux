f3a0cbccec602699a4c653d314577024972a190c
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2019-04-21 01:47:29 -0300

    End Home Component

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -2,8 +2,10 @@ import React, {Component} from 'react';
 import './Home.css';
 import fernando from './fernando.png';
 import raton from './raton.gif';
+import menu from './menu.png';
 //Components
 import Contact from '../Contact/Contact';
+import Logo from './Logo';
 
 
 class Home extends Component{
@@ -11,17 +13,24 @@ class Home extends Component{
     render(){
         return(
             <div className='Home'>
+                  <div className='menu-icon'>
+                    <img alt='menu' src={menu}></img>
+                  </div>
                   <div className='Title'>
                     <h1>Fernando<br></br>Oviedo</h1>
                     <h4>2019</h4>
                   </div>
-                    
-                  <Contact/>
+                  <div className='Logo-Home'>
+                    <Logo/>
+                  </div>
+                  <div className='Btn-Contact'>
+                    <Contact/>
+                  </div>
                   <div className='Dev-img'>
-                    <img src={fernando}></img>
+                    <img alt='FernandoOviedo' src={fernando}></img>
                   </div>
                   <div className='Down'>
-                    <a href='#banner'><img src={raton}></img></a>
+                    <a href='#banner'><img alt='mouse' src={raton}></img></a>
                   </div>
             </div>
         )
