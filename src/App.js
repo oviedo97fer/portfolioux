d28ca3aa82d707c85c7fbfdec4a2f1b66017195d
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2019-04-21 03:25:45 -0300

    Toggle Menu

--- src/App.js
+++ src/App.js
@@ -7,17 +7,34 @@ import Menu from './Components/Menu/Menu';
 import menu from './menu.png';
 
 class App extends Component {
-  menuHandle(){
-    alert('Gola');
+  constructor(){
+    super()
+    this.state = {
+      displayMenu: false,
+    };
+    this.showMenu = this.showMenu.bind(this);
+    this.hideMenu = this.hideMenu.bind(this);
+  }
+  showMenu(e){
+   this.setState({ displayMenu: true });
+   document.addEventListener('click', this.hideMenu);
+   document.removeEventListener('click', this.showMenu);
+  }
+  hideMenu(e){
+    this.setState({displayMenu: false})
+    document.removeEventListener('click', this.hideMenu);
+    document.addEventListener('click', this.showMenu);
   }
   render() {
+    const { displayMenu } = this.state;
+
     return (
       <div className="App">
         {/*menu btn*/}
-         <div onClick={this.menuHandle} className='menu-icon'>
-           <img alt='menu' src={menu}></img>
+        <div className='menu-icon' onClick={this.showMenu}>
+          <img alt='menu' src={menu}></img>
         </div>
-        <Menu/>
+        {displayMenu && <Menu/>}
         <Home/>
         <Banner/>
       </div>
