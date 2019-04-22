4f9add9139e61610ff521672f2ca0a36e8a53c59
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2019-04-21 21:36:09 -0300

    Profile component finished

--- src/Components/Skills/Profile/Profile.js
+++ src/Components/Skills/Profile/Profile.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Profile.css';
 
+import Contact from '../../Contact/Contact';
+
 import profile from './profile.png';
 const Profile = ()=>{
     return(
@@ -8,6 +10,10 @@ const Profile = ()=>{
                 <img src={profile} alt='profile'></img>
                 <h1>WHO I AM?</h1>
                 <p>I am a autolearning front-end developer living in <span>Buenos Aires, ARG.</span></p>
+                <div className='contact-profile'>
+                    <Contact/>
+                </div>
+                
             </div>
     )
 }
