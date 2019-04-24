da389d9f01490e5ca4db63a04978727b79856beb
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2019-04-24 03:05:08 -0300

    Updates

---
+++ precache-manifest.b726e21ee624f8dcc9a1d880978ac635.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "28b38864fe888353531bebc3c80efe8a",
+    "url": "/portfolioux/static/media/wolf.28b38864.png"
+  },
+  {
+    "revision": "4da52ba67a114ab2df0d516382fd423a",
+    "url": "/portfolioux/static/media/raton.4da52ba6.gif"
+  },
+  {
+    "revision": "703ef395496f1904b27adbafb3b20023",
+    "url": "/portfolioux/static/media/profile.703ef395.png"
+  },
+  {
+    "revision": "e55d00b8e281b2321a2954d46e49260b",
+    "url": "/portfolioux/static/media/fernando.e55d00b8.png"
+  },
+  {
+    "revision": "e837c9346331dd2a1d32",
+    "url": "/portfolioux/static/js/runtime~main.5c02630d.js"
+  },
+  {
+    "revision": "a72db25af4d40fd241c4",
+    "url": "/portfolioux/static/js/main.bd9c064b.chunk.js"
+  },
+  {
+    "revision": "718e923f5497750b307b",
+    "url": "/portfolioux/static/js/2.1ea78b79.chunk.js"
+  },
+  {
+    "revision": "a72db25af4d40fd241c4",
+    "url": "/portfolioux/static/css/main.23ad5d4e.chunk.css"
+  },
+  {
+    "revision": "155faa1a2b9923365a3e7931b285cf68",
+    "url": "/portfolioux/index.html"
+  }
+];
