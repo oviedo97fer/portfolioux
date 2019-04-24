9436a3b4cc88e216dfa63db8f02d29148046ccfa
Fernando Oviedo <fernando.n.oviedo@gmail.com>
2019-04-24 02:35:41 -0300

    Updates

---
+++ precache-manifest.58357a4eded7444129fe14ae6389f834.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "28b38864fe888353531bebc3c80efe8a",
+    "url": "/static/media/wolf.28b38864.png"
+  },
+  {
+    "revision": "4da52ba67a114ab2df0d516382fd423a",
+    "url": "/static/media/raton.4da52ba6.gif"
+  },
+  {
+    "revision": "703ef395496f1904b27adbafb3b20023",
+    "url": "/static/media/profile.703ef395.png"
+  },
+  {
+    "revision": "e55d00b8e281b2321a2954d46e49260b",
+    "url": "/static/media/fernando.e55d00b8.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b2a1c52d2a027d534fc2",
+    "url": "/static/js/main.b9c33a2e.chunk.js"
+  },
+  {
+    "revision": "718e923f5497750b307b",
+    "url": "/static/js/2.1ea78b79.chunk.js"
+  },
+  {
+    "revision": "b2a1c52d2a027d534fc2",
+    "url": "/static/css/main.9949cfa3.chunk.css"
+  },
+  {
+    "revision": "be7584f3b19fe0b9de6b1f12012f6c6d",
+    "url": "/index.html"
+  }
+];
--- src/Components/Banner/LoaderBar/LoaderBar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const LoaderBar = ()=>{
-    return(
-        <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
-    )
-}
-export default LoaderBar;
